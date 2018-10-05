94960107b529aa50c592b0c3d799c0b5e31557a9
Mikael Mortensen <mikaem@math.uio.no>
2018-10-05 16:35:11 +0300

    Adding module load to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+module load cray-fftw
 export PREFIX="/scratch/x_mortenm/Software/cray"
 ./configure  --enable-cray --enable-oned --enable-fftw --with-fftw=$FFTW_DIR/.. --enable-measure --prefix=$PREFIX --exec-prefix=$PREFIX FC=ftn CC=cc
 make
